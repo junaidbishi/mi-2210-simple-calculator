95b797c1e3ecbbab7c9d3dfdbd73fc156552943b
junaid bishi <junaidbishi02@gmail.com>
2024-04-01 13:32:09 +0500

    cli simple calculator

---
+++ main.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import chalk from "chalk"
+import inquirer from "inquirer";
+const answer= await inquirer.prompt([
+{message: "enter first number", type: "number", name: "firstnumber"},
+{message: "enter second number", type: "number",name:"secondnumber"},
+{
+    message: "select one of the operation to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["subtraction","addition","multiplication","division"],
+},
+]);
+//conditional operator
+if(answer.operator === "addition"){
+    console.log(chalk.gray(answer.firstnumber + answer.secondnumber));
+}else if(answer.operator === "subtraction"){
+    console.log(chalk.gray (answer.firstnumber - answer.secondnumber));
+}else if(answer.operator === "multiplication"){
+    console.log(chalk.gray(answer.firstnumber * answer.secondnumber));
+}else if(answer.operator === "division"){
+    console.log(chalk.gray(answer.firstnumber / answer.secondnumber));
+}else{
+    console.log(chalk.red("please select valid number"));
+}
+
